052fb7e1e16ec847510ca3bfc8426907b1d73527
doui445 <76111684+doui445@users.noreply.github.com>
2022-11-18 17:51:32 +0100

    Ajout de documentation + modif appel interface
    utilisation de l'interface ISymptomReader qui avait été oublié dans le main

---
+++ Symptoms.java
@@ -0,0 +1,39 @@
+package com.hemebiotech.analytics;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class Symptoms implements ISymptomsList {
+    public Symptoms () {
+    }
+    @Override
+    public SortedMap<String, Integer> listSymptoms(List<String> datas) {
+		SortedMap<String, Integer> result = new TreeMap<String,Integer>();
+        for (String symptom : datas) {
+            if (result.get(symptom) == null) {
+                result.put(symptom, 1);
+            } else {
+                result.put(symptom, result.get(symptom)+1);
+            }
+        }
+        return result;
+    }
+    @Override
+    public void sendDatas (String filepath, List<String> datas) {
+        if (filepath != null) {
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(filepath, false));
+                for (SortedMap.Entry<String, Integer> entry : listSymptoms(datas).entrySet()) {
+                    writer.write(entry.getKey() + " = " + entry.getValue());
+                    writer.newLine();
+                } writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
